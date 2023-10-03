a83f2f7eafd914cd8dc835b5f657d93900eab3a5
Jordan Kingsley <jkngsly@gmail.com>
2023-10-03 15:29:53 -0500

    Search results iteration and props for Position component

--- src/components/Position.vue
+++ src/components/Position.vue
@@ -1,38 +1,39 @@
-<script setup>
-import { ref } from "vue";
-
-const count = ref(0);
+<script>
+export default {
+  props: ["position"],
+  setup(props) {},
+  data() {
+    return {
+      ...this.position,
+    };
+  },
+};
 </script>
 
 <template>
-  <div class="position box rounded">
+  <div
+    class="position box rounded"
+    v-bind:class="active ? 'position-active' : ''"
+  >
     <div class="position-top">
       <div class="position-company-logo">
         <img src="../assets/aws.png" />
       </div>
       <div class="position-details">
-        <span class="position-title">Senior DevOps Engineer</span><br />
-        <span class="position-company">Amazon Web Services</span>
+        <span class="position-title">{{ title }}</span
+        ><br />
+        <span class="position-company">{{ company }}</span>
       </div>
       <div class="position-details-right">
-        <span class="position-salary">125,000 - 175,000</span>
+        <span class="position-salary">{{ salary }}</span>
       </div>
       <div class="position-details-right">
-        <span class="position-location">United States (Remote)</span>
+        <span class="position-location">{{ location }}</span>
       </div>
     </div>
     <div class="position-description">
       <span>Job Description</span>
-      <p>
-        Vivamus ut tempor purus. Cras nec lorem feugiat, ullamcorper diam vitae,
-        faucibus lorem. Pellentesque id efficitur sapien. Integer eleifend,
-        lectus at viverra posuere, ligula ex cursus urna, efficitur varius urna
-        elit at ex. Praesent pulvinar est ut purus lacinia, ut dapibus odio
-        commodo. Donec gravida vestibulum diam, vitae laoreet lacus aliquet sed.
-        Orci varius natoque penatibus et magnis dis parturient montes, nascetur
-        ridiculus mus. Etiam dapibus iaculis sapien a ornare. Morbi congue
-        tortornis....
-      </p>
+      <p>{{ description }}</p>
     </div>
   </div>
 </template>
--- src/components/SearchResults.vue
+++ src/components/SearchResults.vue
@@ -1,22 +1,68 @@
-<script setup>
-import { ref } from "vue";
+<script>
 import Position from "./Position.vue";
 
-defineProps({
-  msg: String,
-});
-
-const count = ref(0);
+export default {
+  components: { Position },
+  data() {
+    return {
+      activeChild: false,
+      results: [
+        {
+          active: false,
+          title: "Senior DevOps Engineer",
+          company: "Amazon Web Services",
+          salary: "125,000 - 175,000",
+          location: "United States (Remote)",
+          description: `Vivamus ut tempor purus. Cras nec lorem feugiat, ullamcorper diam vitae,
+        faucibus lorem. Pellentesque id efficitur sapien. Integer eleifend,
+        lectus at viverra posuere, ligula ex cursus urna, efficitur varius urna
+        elit at ex. Praesent pulvinar est ut purus lacinia, ut dapibus odio
+        commodo. Donec gravida vestibulum diam, vitae laoreet lacus aliquet sed.
+        Orci varius natoque penatibus et magnis dis parturient montes, nascetur
+        ridiculus mus. Etiam dapibus iaculis sapien a ornare. Morbi congue
+        tortornis....`,
+        },
+        {
+          active: false,
+          title: "Senior DevOps Engineer",
+          company: "Amazon Web Services",
+          salary: "125,000 - 175,000",
+          location: "United States (Remote)",
+          description: `Vivamus ut tempor purus. Cras nec lorem feugiat, ullamcorper diam vitae,
+        faucibus lorem. Pellentesque id efficitur sapien. Integer eleifend,
+        lectus at viverra posuere, ligula ex cursus urna, efficitur varius urna
+        elit at ex. Praesent pulvinar est ut purus lacinia, ut dapibus odio
+        commodo. Donec gravida vestibulum diam, vitae laoreet lacus aliquet sed.
+        Orci varius natoque penatibus et magnis dis parturient montes, nascetur
+        ridiculus mus. Etiam dapibus iaculis sapien a ornare. Morbi congue
+        tortornis....`,
+        },
+        {
+          active: false,
+          title: "Senior DevOps Engineer",
+          company: "Amazon Web Services",
+          salary: "125,000 - 175,000",
+          location: "United States (Remote)",
+          description: `Vivamus ut tempor purus. Cras nec lorem feugiat, ullamcorper diam vitae,
+        faucibus lorem. Pellentesque id efficitur sapien. Integer eleifend,
+        lectus at viverra posuere, ligula ex cursus urna, efficitur varius urna
+        elit at ex. Praesent pulvinar est ut purus lacinia, ut dapibus odio
+        commodo. Donec gravida vestibulum diam, vitae laoreet lacus aliquet sed.
+        Orci varius natoque penatibus et magnis dis parturient montes, nascetur
+        ridiculus mus. Etiam dapibus iaculis sapien a ornare. Morbi congue
+        tortornis....`,
+        },
+      ],
+    };
+  },
+  methods() {},
+};
 </script>
 
 <template>
   <div id="results">
     <span>Showing 20 of 70 Results</span>
-    <Position />
-    <Position />
-    <Position />
-    <Position />
-    <Position />
+    <Position v-for="result in results" :position="result" />
   </div>
 </template>
 
