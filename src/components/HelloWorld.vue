9e286f18f7300e2f758cbc0169c4bc5ffffb92d4
Jordan Kingsley <jkngsly@gmail.com>
2023-10-02 19:54:32 -0500

    Initial push, Vite scaffold, starter DOM/SASS

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from "vue";
+
+defineProps({
+  msg: String,
+});
+
+const count = ref(0);
+</script>
+
+<template>
+  <header>
+    <a href="#" id="header-logo"><h1>yajb</h1></a>
+    <nav>
+      <a href="#" class="active">Find Jobs</a>
+      <a href="#">My Jobs</a>
+      <a href="#">Menu Item</a>
+      <a href="#">Menu Item</a>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
