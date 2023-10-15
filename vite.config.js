521a78551b2789722fe0266b400ed55c8286c526
Jordan Kingsley <jkngsly@gmail.com>
2023-10-15 11:41:21 -0500

    Minimize vite config

--- vite.config.js
+++ vite.config.js
@@ -2,47 +2,32 @@ import { fileURLToPath, URL } from "url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-// https://vitejs.dev/config/
+const getPaths = (paths) => {
+  let a = [];
+  paths.forEach((p) => {
+    a.push({
+      find: "@" + p,
+      replacement: fileURLToPath(
+        new URL("./src" + (p.length ? "/" + p : ""), import.meta.url)
+      ),
+    });
+  });
+
+  return a;
+};
+
 export default defineConfig({
   plugins: [vue()],
   resolve: {
-    alias: [
-      {
-        find: "@",
-        replacement: fileURLToPath(new URL("./src", import.meta.url)),
-      },
-      {
-        find: "@assets",
-        replacement: fileURLToPath(new URL("./src/assets", import.meta.url)),
-      },
-      {
-        find: "@components",
-        replacement: fileURLToPath(
-          new URL("./src/components", import.meta.url)
-        ),
-      },
-      {
-        find: "@config",
-        replacement: fileURLToPath(new URL("./src/config", import.meta.url)),
-      },
-      {
-        find: "@directives",
-        replacement: fileURLToPath(
-          new URL("./src/directives", import.meta.url)
-        ),
-      },
-      {
-        find: "@services",
-        replacement: fileURLToPath(new URL("./src/services", import.meta.url)),
-      },
-      {
-        find: "@store",
-        replacement: fileURLToPath(new URL("./src/store", import.meta.url)),
-      },
-      {
-        find: "@views",
-        replacement: fileURLToPath(new URL("./src/views", import.meta.url)),
-      },
-    ],
+    alias: getPaths([
+      "",
+      "assets",
+      "components",
+      "config",
+      "directives",
+      "services",
+      "store",
+      "views",
+    ]),
   },
 });
